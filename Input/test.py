e297b00ecf4cf3edbaa2e10d78538f35c02553f1
Florian Staub <florian.staub@googlemail.com>
2018-04-18 21:34:27 +0200

    pyslha included

---
+++ Input/test.py
@@ -0,0 +1,86 @@
+settings_file="MSSM.py"
+blocks=['MODSEL','SMINPUTS','MINPAR','SPhenoInput']
+
+scan="FirstTry"
+scan_type="Grid"
+append_points= False
+include_HiggsBounds= True
+include_HiggsSignals= True
+include_MicrOmegas= True
+
+MODSEL=[
+    ['1',{'value': 1}],
+    ['2',{'value': 1}],  
+    ['6',{'value': 1}]
+    ]
+
+SMINPUTS=[
+ ['2', {'value': 1.166370E-05}],    
+ ['3', {'value': 1.187000E-01}],    
+ ['4', {'value': 9.118870E+01}],    
+ ['5', {'value': 4.180000E+00}],    
+ ['6', {'value': 1.735000E+02}],    
+ ['7', {'value': 1.776690E+00}] ]   
+
+MINPAR=[
+    ['1',{'min':200., 'max':2000.,'steps':4}],
+    ['2',{'value':"MINPAR['1']"}],    
+#    ['2',{'value':500.}],    
+    ['3',{'min':5.,'max':50.,'steps':3}],     
+    ['4',{'value': 1.}],
+    ['5',{'value': 0.}]        
+    ]
+
+SPhenoInput=[
+['  1',{'value': -1          }],
+['  2',{'value':  0          }],
+['  7',{'value':  0          }],
+['  8',{'value':  3          }],
+['  9',{'value':  1          }],
+[' 10',{'value':  0          }],
+[' 11',{'value': 0           }],
+[' 13',{'value': 1           }],
+[' 14',{'value': 0           }],
+[' 12',{'value': 1.000E-04   }],
+[' 15',{'value': 1.000E-30   }],
+[' 16',{'value': 1           }],
+[' 19',{'value': -2          }],
+[' 31',{'value': -1          }],
+[' 32',{'value': 0           }],
+[' 34',{'value': 1.000E-04   }],
+[' 35',{'value': 40          }],
+[' 36',{'value': 5           }],
+[' 37',{'value': 1           }],
+[' 38',{'value': 2           }],
+[' 50',{'value': 1           }], 
+[' 51',{'value': 0           }],
+[' 52',{'value': 0           }],
+[' 55',{'value': 0           }],
+[' 57',{'value': 0           }],
+[' 65',{'value': 1           }],
+[' 66',{'value': 1           }],
+[' 67',{'value': 1           }],
+[' 75',{'value': 1           }],
+[' 76',{'value': 1           }],
+[' 77',{'value': 0           }],
+[' 78',{'value': 0           }],
+[' 79',{'value': 1           }],
+[' 86',{'value': 0.          }],
+['510',{'value': 0.          }],
+['515',{'value': 0           }],
+['520',{'value': 1.          }], 
+['521',{'value': 1.          }],
+['525',{'value': 0.          }],
+['530',{'value': 1.          }],
+['550',{'value': 0.          }]]
+
+
+
+
+yu=[
+    ['2 2',{'value': "yu['1 1']"}],
+    ['1 1',{'min': 0., 'max':1., 'steps':3}],  
+    ['3 3',{'value': "yu['1 1']"}]
+    ]
+
+
---
+++ ModelsSettings/MSSM.py
@@ -0,0 +1,12 @@
+SPheno_binary = "~/Documents/Uni/Work/SPhenos/Test_4.0.3_4.13.0/bin/SPhenoMSSM"
+SPheno_input= "LesHouches.in.MSSM"
+SPheno_output = "SPheno.spc.MSSM"
+
+Micromegas_binary = "~/Documents/Uni/Work/Software/micromegas_2.4.5/BLSSM/CalcOmega"
+Micromegas_input = "SPheno.spc.BLSSM"
+Micromegas_output = "omg.out"
+Micromegas_candidate = 1000012
+
+HiggsBounds_call = "/home/fnstaub/Documents/Uni/Work/Software/HiggsBounds-4.3.1/HiggsBounds LandH effC 3 1"
+
+ 
